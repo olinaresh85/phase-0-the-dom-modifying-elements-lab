61fed5de93c506d6fd6ada5911deceba305f68f4
Naresh Oli <olinaresh85@gmail.com>
2023-04-08 18:02:54 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+                  document.body.appendChild(newHeader);
+
+newHeader.id = 'victory'
+newHeader.textContent = 'Naresh is the champion'
+
